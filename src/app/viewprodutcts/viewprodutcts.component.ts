43d85726b927560ef838b4be96522bb0d8bc7bda
Sathishsat0306 <31485747+Sathishsat0306@users.noreply.github.com>
2017-09-02 14:36:22 +0530

    chore: initial commit from @angular/cli

---
+++ src/app/viewprodutcts/viewprodutcts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewprodutcts',
+  templateUrl: './viewprodutcts.component.html',
+  styleUrls: ['./viewprodutcts.component.css']
+})
+export class ViewprodutctsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
