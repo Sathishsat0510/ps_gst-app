3b09a7d9246cee752a8a9d3e02dfffba7f36f714
Sathishsat0306 <31485747+Sathishsat0306@users.noreply.github.com>
2017-09-03 09:49:49 +0530

    backendfinish

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,6 @@
 const express=require('express');
 const router=express.Router();
 var mysql = require('mysql');
-const Products=require('../models/Products');
 
 router.get('/product',function (req,res,next) {
 
